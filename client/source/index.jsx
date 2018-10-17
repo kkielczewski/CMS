e2338bb0f19561af72f835869fb603c7f8ab523a
Unknown <kam.kielczewski@gmail.com>
2018-10-17 18:09:22 +0200

    asd

--- client/source/index.jsx
+++ client/source/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureStore from './redux';
 import Main from './containers/main';
 import Promise from 'promise-polyfill';
 import './styles/base.scss';
 
-import 'jquery/src/jquery';
-import '../node_modules/toastr/build/toastr.min.css';
+const store = configureStore();
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
 ReactDOM.render(
-    <Main />,
+    <Provider store={store} >
+    <BrowserRouter>
+      <div className="app-container" style={{ height: '100%' }} >
+        <Main />
+      </div>
+    </BrowserRouter>
+  </Provider>,
     document.querySelector('#root')
 );
