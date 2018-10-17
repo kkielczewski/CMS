40906872c3a7fef3931094b8e7de922e9d2b0330
Kamil <xkielczewski@gmail.com>
2018-10-17 16:27:04 +0200

    asd

--- client/build-utils/addons/webpack.bundleanalyzer.js
+++
@@ -1,9 +0,0 @@
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-module.exports = {
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'server'
-    })
-  ]
-};
--- client/source/app/constants/key-constants.js
+++
@@ -1,4 +0,0 @@
-// Demo test Stripe publishable key (replace with your own)
-/* eslint-disable */
-export const STRIPE_PUBLIC_KEY = 'pk_test_6pRNASCoBOKtIshFeQd4XMUh';
-/* eslint-enable */
