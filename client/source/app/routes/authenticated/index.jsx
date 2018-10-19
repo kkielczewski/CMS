c1e2f0a285fa01168cbbf0fd04c3101696f5d24a
Kamil <xkielczewski@gmail.com>
2018-10-19 15:14:57 +0200

    asd

--- client/source/app/routes/authenticated/index.jsx
+++ client/source/app/routes/authenticated/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Articles from '../../components/articles';
+import Login from '../../components/authentication/login';
 
 const AuthenticatedRoutes = () => (
   <Switch>
-    <Route exact path="/dashboard" component={() => <div>Welcome to the dashboard</div>} />
+    <Route exact path="/dashboard" component={() => <div><Login /></div>} />
     <Route exact path="/articles" component={() => <Articles />} />
   </Switch>
 );
