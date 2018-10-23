14c6022f28e4a7d755330b9270fabdb016542f35
Unknown <kam.kielczewski@gmail.com>
2018-10-23 21:02:20 +0200

    asd

--- routes/upload.js
+++ routes/upload.js
@@ -1,5 +1,5 @@
 const Router = require('koa-router');
-const multer = require('multer');
+const multer = require('koa-multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -19,14 +19,6 @@ const upload = multer({ storage });
 const router = new Router();
 
 router.post('/upload', upload.single('image'), async (ctx, next) => {
-  console.log('/upload');
-  if (ctx.req.file) {
-    ctx.res.json({
-      imageUrl: `uploads/${ctx.req.file.filename}`
-    });
-  } else {
-    ctx.status('409').json('No Files to Upload.');
-  }
 });
 
 module.exports = router;
