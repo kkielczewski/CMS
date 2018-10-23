a80846093187ecd66b1444bdbce7d44663622f08
Unknown <kam.kielczewski@gmail.com>
2018-10-23 19:03:50 +0200

    asd

--- routes/upload.js
+++ routes/upload.js
@@ -3,18 +3,23 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
+    console.log('destination');
     cb(null, 'client/dist/uploads');
   },
   filename: (req, file, cb) => {
+    console.log('originalname');
     cb(null, file.originalname);
   }
 });
+
+console.log('upload');
 
 const upload = multer({ storage });
 
 const router = new Router();
 
 router.post('/upload', upload.single('image'), async (ctx, next) => {
+  console.log('/upload');
   if (ctx.req.file) {
     ctx.res.json({
       imageUrl: `uploads/${ctx.req.file.filename}`
